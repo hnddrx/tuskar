86ee8fe8375f13bbeaba86e30d0791c60533b41e
hnddrx <macayanwren@gmail.com>
2026-08-25 17:04:45 +0800

    clerk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { TaskProvider } from "@/context/TaskContext";
 import AppShell from "@/components/AppShell";
 
@@ -12,9 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased">
       <body className="min-h-full">
-        <TaskProvider>
-          <AppShell>{children}</AppShell>
-        </TaskProvider>
+        <ClerkProvider>
+          <TaskProvider>
+            <AppShell>{children}</AppShell>
+          </TaskProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
